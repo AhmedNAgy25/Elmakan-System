da6c250fac63ec733987bc95461cde054addd3c5
Ahmed NAgy <ahmednagy25t@gmail.com>
2025-08-16 20:26:49 +0300

    edit projext structure to follow RSC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Navbar from '@/components/client/Navbar'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-      page
+      <Navbar></Navbar>
     </div>
   )
 }
